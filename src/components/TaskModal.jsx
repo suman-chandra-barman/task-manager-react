766c9f3e39f53bd3ce09a71425818b4146f9882d
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2024-10-30 18:33:43 +0600

    [Change] task detail modal page design

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -16,33 +16,44 @@ function TaskModal({task, selectedTask, setSelectedTask, todoData, setTodoData})
         setTodoData(updatedTodoData);
     }
 
+    function handleChecklist(){
+        
+    }
+
     return(
         <div id="task_modal" className="task-modal">
             <div className="task-modal-content">
-                <span id="modal_close_btn" onClick={closeTaskModal} className="close">&times;</span>
-                <input 
-                    onChange={
-                        function(event){
-                            return handleTaskModalTitleChange(event)
-                        }
-                    } 
-                    id="modal_task_title" 
-                    className="modal-task-title" 
-                    type="text" value={task.title} 
-                    placeholder="Enter task name"
-                />
-                <textarea 
-                     onChange={
-                        function(event){
-                            return handleTaskModalDescriptionChange(event)
-                        }
-                    } 
-                    id="modal_task_description" 
-                    value={task.description} 
-                    rows="4" 
-                    placeholder="Enter your task description..."
-                >
-                </textarea>
+                <div className="task-modal-left-side">
+                    <input 
+                        onChange={
+                            function(event){
+                                return handleTaskModalTitleChange(event)
+                            }
+                        } 
+                        id="modal_task_title" 
+                        className="modal-task-title" 
+                        type="text" value={task.title} 
+                        placeholder="Enter task name"
+                    />
+                    <textarea 
+                        onChange={
+                            function(event){
+                                return handleTaskModalDescriptionChange(event)
+                            }
+                        } 
+                        id="modal_task_description" 
+                        value={task.description} 
+                        rows="4" 
+                        placeholder="Enter your task description..."
+                    >
+                    </textarea>
+                </div>
+                <div className="task-modal-right-side">
+                    <span id="modal_close_btn" onClick={closeTaskModal} className="close">&times;</span>
+                    <div>
+                        <button onClick={handleChecklist} className="task-modal-btn">Checklist</button>
+                    </div>
+                </div>
             </div>
         </div>
 
